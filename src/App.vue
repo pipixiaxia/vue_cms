2743145aaf51b6faf451230727c353198b4c4c8a
pipixiaxia <1466147148@qq.com>
2018-10-13 11:52:26 +0800

    第二次修改了App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <span class="mui-icon mui-icon-home"></span>
       <span class="mui-tab-label">首页</span>
     </router-link>
-    <router-link class="mui-tab-item" to="/">
+    <router-link class="mui-tab-item" to="/member">
       <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
       <span class="mui-tab-label">消息</span>
     </router-link>
