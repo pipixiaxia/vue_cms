0fa782f020392299dce3967c59cebe0c4c5c539a
pipixiaxia <1466147148@qq.com>
2018-10-16 11:25:16 +0800

    实现九宫格和tab动画

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
- <div class="app-container">
-  <!-- 顶部Header区域 -->
-  <mt-header fixed title="CMS"></mt-header>
-  <!-- 中间router-view区域 -->
-  <router-view></router-view>
-  <!-- 底部Tabbar区域 -->
-  <nav class="mui-bar mui-bar-tab">
-    <router-link class="mui-tab-item" to="/home">
-      <span class="mui-icon mui-icon-home"></span>
-      <span class="mui-tab-label">首页</span>
-    </router-link>
-    <router-link class="mui-tab-item" to="/member">
-      <span class="mui-icon mui-icon-contact"></span>
-      <span class="mui-tab-label">会员</span>
-    </router-link>
-    <router-link class="mui-tab-item" to="/cart">
-      <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
-      <span class="mui-tab-label">购物车</span>
-    </router-link> 
-    <router-link class="mui-tab-item" to="/search">
-      <span class="mui-icon mui-icon-search"></span>
-      <span class="mui-tab-label">搜索</span>
-    </router-link>
-  </nav>
+  <div class="app-container">
+    <!-- 顶部Header区域 -->
+    <mt-header fixed title="CMS"></mt-header>
+    <!-- 中间router-view区域 -->
+    <transition>
+      <router-view></router-view>
+    </transition>
+    <!-- 底部Tabbar区域 -->
+    <nav class="mui-bar mui-bar-tab">
+      <router-link class="mui-tab-item" to="/home">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/member">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </router-link>
+      <router-link class="mui-tab-item" to="/cart">
+        <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+        <span class="mui-tab-label">购物车</span>
+      </router-link> 
+      <router-link class="mui-tab-item" to="/search">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </router-link>
+    </nav>
  </div>
 </template>
 
@@ -39,5 +41,26 @@ export default {
 .app-container {
   padding-top: 40px;
   padding-bottom: 50px;
+  overflow-x: hidden;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all ease 0.2s;
+}
+
+body,
+nav.mui-bar.mui-bar-tab {
+  background-color: #fff;
 }
 </style>
--- src/components/tabbar/home.vue
+++ src/components/tabbar/home.vue
@@ -5,6 +5,44 @@
           <img :src="apis + item.img" alt="">
         </mt-swipe-item>
       </mt-swipe>
+      <ul class="mui-table-view mui-grid-view mui-grid-9">
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu1.png" alt="">
+              <div class="mui-media-body">新闻资讯</div>
+            </a>
+          </li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu2.png" alt="">
+              <div class="mui-media-body">图片分享</div>
+            </a>
+          </li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu3.png" alt="">
+              <div class="mui-media-body">商品购买</div>
+            </a>
+          </li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu4.png" alt="">
+              <div class="mui-media-body">留言反馈</div>
+            </a>
+          </li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu5.png" alt="">
+              <div class="mui-media-body">视频专区</div>
+            </a>
+          </li>
+          <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+            <a href="#">
+              <img height="60" src="../../../static/img/menu6.png" alt="">
+              <div class="mui-media-body">联系我们</div>
+            </a>
+          </li>
+      </ul>
     </div>
 </template>
 
@@ -38,6 +76,16 @@ export default {
   img {
     width: 100%;
     height: 100%;
+  }
+}
+
+.mui-table-view.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  li {
+    border: none;
+    .mui-media-body {
+      font-size: 14px;
+    }
   }
 }
 </style>
